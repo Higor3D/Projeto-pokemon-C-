34d8e5eb13a8367b0e187e351a79ac440e1779d2
Higor3D <higorgomes07@gmail.com>
2023-02-20 19:32:57 -0300

    Adicionar arquivos de projeto.

---
+++ Digimon.3/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Digimon._3
+{
+    partial class Pokemons
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPikachu = new System.Windows.Forms.RadioButton();
+            this.btnOddish = new System.Windows.Forms.RadioButton();
+            this.btnWeedle = new System.Windows.Forms.RadioButton();
+            this.btnRatata = new System.Windows.Forms.RadioButton();
+            this.btnCartepie = new System.Windows.Forms.RadioButton();
+            this.btnPsyduck = new System.Windows.Forms.RadioButton();
+            this.btnGrowlithe = new System.Windows.Forms.RadioButton();
+            this.btnZubat = new System.Windows.Forms.RadioButton();
+            this.lblname = new System.Windows.Forms.Label();
+            this.btnAbra = new System.Windows.Forms.RadioButton();
+            this.foto = new System.Windows.Forms.PictureBox();
+            this.btnMankey = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(-9, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(908, 534);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnPikachu
+            // 
+            this.btnPikachu.AutoSize = true;
+            this.btnPikachu.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPikachu.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnPikachu.Location = new System.Drawing.Point(110, 314);
+            this.btnPikachu.Name = "btnPikachu";
+            this.btnPikachu.Size = new System.Drawing.Size(112, 28);
+            this.btnPikachu.TabIndex = 24;
+            this.btnPikachu.Text = "Pikachu";
+            this.btnPikachu.UseVisualStyleBackColor = true;
+            // 
+            // btnOddish
+            // 
+            this.btnOddish.AutoSize = true;
+            this.btnOddish.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOddish.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnOddish.Location = new System.Drawing.Point(110, 291);
+            this.btnOddish.Name = "btnOddish";
+            this.btnOddish.Size = new System.Drawing.Size(100, 28);
+            this.btnOddish.TabIndex = 23;
+            this.btnOddish.Text = "Oddish";
+            this.btnOddish.UseVisualStyleBackColor = true;
+            // 
+            // btnWeedle
+            // 
+            this.btnWeedle.AutoSize = true;
+            this.btnWeedle.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWeedle.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnWeedle.Location = new System.Drawing.Point(110, 268);
+            this.btnWeedle.Name = "btnWeedle";
+            this.btnWeedle.Size = new System.Drawing.Size(100, 28);
+            this.btnWeedle.TabIndex = 22;
+            this.btnWeedle.Text = "Weedle";
+            this.btnWeedle.UseVisualStyleBackColor = true;
+            // 
+            // btnRatata
+            // 
+            this.btnRatata.AutoSize = true;
+            this.btnRatata.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRatata.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnRatata.Location = new System.Drawing.Point(110, 245);
+            this.btnRatata.Name = "btnRatata";
+            this.btnRatata.Size = new System.Drawing.Size(100, 28);
+            this.btnRatata.TabIndex = 21;
+            this.btnRatata.Text = "Ratata";
+            this.btnRatata.UseVisualStyleBackColor = true;
+            // 
+            // btnCartepie
+            // 
+            this.btnCartepie.AutoSize = true;
+            this.btnCartepie.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCartepie.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnCartepie.Location = new System.Drawing.Point(110, 222);
+            this.btnCartepie.Name = "btnCartepie";
+            this.btnCartepie.Size = new System.Drawing.Size(124, 28);
+            this.btnCartepie.TabIndex = 20;
+            this.btnCartepie.Text = "Cartepie";
+            this.btnCartepie.UseVisualStyleBackColor = true;
+            // 
+            // btnPsyduck
+            // 
+            this.btnPsyduck.AutoSize = true;
+            this.btnPsyduck.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPsyduck.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnPsyduck.Location = new System.Drawing.Point(110, 199);
+            this.btnPsyduck.Name = "btnPsyduck";
+            this.btnPsyduck.Size = new System.Drawing.Size(112, 28);
+            this.btnPsyduck.TabIndex = 19;
+            this.btnPsyduck.Text = "Psyduck";
+            this.btnPsyduck.UseVisualStyleBackColor = true;
+            // 
+            // btnGrowlithe
+            // 
+            this.btnGrowlithe.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnGrowlithe.AutoSize = true;
+            this.btnGrowlithe.Checked = true;
+            this.btnGrowlithe.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrowlithe.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnGrowlithe.Location = new System.Drawing.Point(110, 151);
+            this.btnGrowlithe.Name = "btnGrowlithe";
+            this.btnGrowlithe.Size = new System.Drawing.Size(136, 28);
+            this.btnGrowlithe.TabIndex = 18;
+            this.btnGrowlithe.TabStop = true;
+            this.btnGrowlithe.Text = "Growlithe";
+            this.btnGrowlithe.UseVisualStyleBackColor = true;
+            // 
+            // btnZubat
+            // 
+            this.btnZubat.AutoSize = true;
+            this.btnZubat.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZubat.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnZubat.Location = new System.Drawing.Point(110, 176);
+            this.btnZubat.Name = "btnZubat";
+            this.btnZubat.Size = new System.Drawing.Size(88, 28);
+            this.btnZubat.TabIndex = 17;
+            this.btnZubat.Text = "Zubat";
+            this.btnZubat.UseVisualStyleBackColor = true;
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblname.Location = new System.Drawing.Point(110, 461);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(155, 28);
+            this.lblname.TabIndex = 16;
+            this.lblname.Text = "Higor Gomes";
+            // 
+            // btnAbra
+            // 
+            this.btnAbra.AutoSize = true;
+            this.btnAbra.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbra.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnAbra.Location = new System.Drawing.Point(110, 130);
+            this.btnAbra.Name = "btnAbra";
+            this.btnAbra.Size = new System.Drawing.Size(76, 28);
+            this.btnAbra.TabIndex = 15;
+            this.btnAbra.Text = "Abra";
+            this.btnAbra.UseVisualStyleBackColor = true;
+            // 
+            // foto
+            // 
+            this.foto.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.foto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.foto.InitialImage = null;
+            this.foto.Location = new System.Drawing.Point(330, 102);
+            this.foto.Name = "foto";
+            this.foto.Size = new System.Drawing.Size(240, 240);
+            this.foto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.foto.TabIndex = 14;
+            this.foto.TabStop = false;
+            // 
+            // btnMankey
+            // 
+            this.btnMankey.AutoSize = true;
+            this.btnMankey.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMankey.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnMankey.Location = new System.Drawing.Point(110, 107);
+            this.btnMankey.Name = "btnMankey";
+            this.btnMankey.Size = new System.Drawing.Size(100, 28);
+            this.btnMankey.TabIndex = 13;
+            this.btnMankey.Text = "Mankey";
+            this.btnMankey.UseVisualStyleBackColor = true;
+            // 
+            // Pokemons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(901, 529);
+            this.Controls.Add(this.btnPikachu);
+            this.Controls.Add(this.btnOddish);
+            this.Controls.Add(this.btnWeedle);
+            this.Controls.Add(this.btnRatata);
+            this.Controls.Add(this.btnCartepie);
+            this.Controls.Add(this.btnPsyduck);
+            this.Controls.Add(this.btnGrowlithe);
+            this.Controls.Add(this.btnZubat);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.btnAbra);
+            this.Controls.Add(this.foto);
+            this.Controls.Add(this.btnMankey);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Pokemons";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Pokemons_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton btnPikachu;
+        private System.Windows.Forms.RadioButton btnOddish;
+        private System.Windows.Forms.RadioButton btnWeedle;
+        private System.Windows.Forms.RadioButton btnRatata;
+        private System.Windows.Forms.RadioButton btnCartepie;
+        private System.Windows.Forms.RadioButton btnPsyduck;
+        private System.Windows.Forms.RadioButton btnGrowlithe;
+        private System.Windows.Forms.RadioButton btnZubat;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.RadioButton btnAbra;
+        private System.Windows.Forms.PictureBox foto;
+        private System.Windows.Forms.RadioButton btnMankey;
+    }
+}
+
---
+++ Digimon.3/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digimon._3
+{
+    public partial class Pokemons : Form
+    {
+        public Pokemons() => InitializeComponent();
+
+        private void btnMankey_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.mankey1;
+        }
+
+        private void btnAbra_CheckedChanged_1(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.abra1;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            lblname.Text = "Higor Gomes";
+        }
+
+        private void btnGrowlithe_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.growlithe1;
+
+        }
+
+        private void btnZubat_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = (Properties.Resources.zubat1);
+        }
+
+        private void btnPsyduck_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.PSYDUCK1;
+
+        }
+
+        private void btnCartepie_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.cartepie1;
+        }
+
+        private void btnRatata_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.ratata1;
+        }
+
+        private void btnWeedle_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.resources.weedle1;
+        }
+
+        private void btnOddish_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image = Properties.Resources.odissh1;
+        }
+
+        private void btnPikachu_CheckedChanged(object sender, EventArgs e)
+        {
+            foto.Image= Properties.Resources.pikachu1;
+        }
+
+        private void foto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pokemons_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
